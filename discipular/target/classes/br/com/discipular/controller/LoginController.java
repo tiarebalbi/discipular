589d059b4128210460f7e9d2100e1128cc0d4aa8
Lucas Campos <lucasgcampos.contato@gmail.com>
2014-09-15 01:10:26 -0300

    Spring Security OK

--- discipular/target/classes/br/com/discipular/controller/LoginController.java
+++ discipular/target/classes/br/com/discipular/controller/LoginController.java
@@ -2,13 +2,11 @@ package br.com.discipular.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.discipular.service.UsuarioService;
-import br.com.discipular.validator.UsuarioValidator;
 
 @Controller
 public class LoginController {
@@ -17,13 +15,6 @@ public class LoginController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	@Autowired
-	private UsuarioValidator validator;
-	
-	public void initBinder(WebDataBinder binder) {
-		binder.setValidator(validator);
-	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView index() {
